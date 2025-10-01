54f49262951020e80c6db33fa8a9894b9960ee22
khw7385 <khw7385@naver.com>
2025-10-01 15:39:31 +0900

    api 작성

---
+++ src/test/java/ac/lgcns/msa/springrest/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.lgcns.msa.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
